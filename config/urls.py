e00d80cbf88322dcbce58dd151e4c592edc034ee
Rafael Monclova <rafamonclova37@gmail.com>
2026-03-20 10:49:59 +0100

    Fix media

--- config/urls.py
+++ config/urls.py
@@ -48,4 +48,11 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^apiweb/media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
